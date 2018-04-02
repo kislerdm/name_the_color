b17bdd673ff533aac807462d6ad5c0b1e62c2a5f
Dmitry Kisler <admin@dkisler.de>
2018-04-02 04:16:59 +0200

    modif2

--- ui.R
+++ ui.R
@@ -10,8 +10,14 @@ body <- dashboardBody(
                column(2),
                box(background = NULL, solidHeader = T, title = NULL, collapsible = F, width = 8, align = "center",
                    box(background = NULL, solidHeader = F, title = 'Palette', collapsible = F, width = 12, align = "center",
-                       tags$div(id = 'palette_hex', uiOutput('col_palette_sel'), align = 'left'),
-                       colourInput("col_palette", NULL, showColour = "background")
+                       column(9,
+                              tags$div(id = 'palette_hex', uiOutput('col_palette_sel'), align = 'left'),
+                              colourInput("col_palette", NULL, showColour = "background")
+                              ),
+                       column(3, 
+                              textInput('col_name', 'Guess Color by Name', value = "White"),
+                              selectInput('col_name_known', 'Display Color by Name', choices = 'White', multiple = F)
+                       )
                    ),
                    box(background = NULL, solidHeader = F, title = 'Color Settings', collapsible = T, width = 12, align = "center",
                        column(4, 
@@ -23,11 +29,12 @@ body <- dashboardBody(
                                        sliderInput('g_in', label = NULL, min = 0, max = 255, step = 1, ticks = F, value = 255, pre = 'G = '),
                                        sliderInput('b_in', label = NULL, min = 0, max = 255, step = 1, ticks = F, value = 255, pre = 'B = '))
                        )
-                   ), 
-                   box(background = NULL, solidHeader = F, title = 'Color Name', collapsible = T, width = 12, align = "center",
-                       column(6, textInput('col_name', 'Guess Color by Name', value = "White")),
-                       column(6, tags$div(selectInput('col_name_known', 'Display Color by Name', choices = 'White', multiple = F)))
                    )
+                   # , 
+                   # box(background = NULL, solidHeader = F, title = 'Color Name', collapsible = T, width = 12, align = "center",
+                   #     column(6, textInput('col_name', 'Guess Color by Name', value = "White")),
+                   #     column(6, tags$div(selectInput('col_name_known', 'Display Color by Name', choices = 'White', multiple = F)))
+                   # )
                )
              ),
              column(width = 12, tags$div(HTML(paste0(format(Sys.Date(), '%Y'), " © <a href='https://www.dkisler.de' target='blank_'>Dmitry Kisler</a>",
